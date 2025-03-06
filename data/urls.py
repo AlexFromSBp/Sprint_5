ab2a839ea77f6b4f6922b524f55bf216c2a2d377
AlexFromSBp <tribunaleksandra@gmail.com>
2025-03-06 21:05:47 +0300

    Add files via upload

---
+++ data/urls.py
@@ -0,0 +1,6 @@
+class Urls:
+    url_home_page = "https://stellarburgers.nomoreparties.site/"                       # Главная
+    url_personal_account = "https://stellarburgers.nomoreparties.site/login"          # Личный кабинет
+    url_profile = "https://stellarburgers.nomoreparties.site/account/profile"         # Профиль
+    url_register = "https://stellarburgers.nomoreparties.site/register"               # Зарегистрироваться
+    url_resert_password = "https://stellarburgers.nomoreparties.site/forgot-password" # Восстановить пароль
